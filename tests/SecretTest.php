d08270b0646be1bb7368a9690f1074cb9b342d7c
Guido Falsi <mad@madpilot.net>
2018-01-27 18:16:12 +0100

    Add skeleton file.

---
+++ tests/SecretTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace otputil\test\secret;
+
+use mad\yii2-otputil\Secret;
+
+class SecretTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->mockApplication();
+    }
+
+    // Tests:
+
+    testGenSecret()
+    {
+    }
+}
