3f09ba47bd843079b13021e3f97f15bac0c8ef22
Guido Falsi <mad@madpilot.net>
2018-01-29 11:19:18 +0100

    Add skeleton of new checks.

---
+++ tests/OTPTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace yii2-otputil\test\OTP;
+
+use chillerlan\Authenticator\Base32;
+use mad\yii2-otputil\OTP;
+use mad\yii2-otputil\Secret;
+
+class SecretTest extends TestCase
+{
+    protected $base32;
+    protected $s;
+    protected $p;
+
+    protected function setUp()
+    {
+        $this->mockApplication();
+        $this->base32 = new Base32();
+        $this->s = new Secret();
+        $this->p = new OTP();
+    }
+
+    // Tests:
+
+    public function testGetCheckOTP()
+    {
+        // to be implemented:
+        // generate one OTP and check it
+    }
+}
