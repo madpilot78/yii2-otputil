2afb2e7ba894c05754bc1fa0c550864c1c74773e
Guido Falsi <mad@madpilot.net>
2018-02-01 10:50:43 +0100

    Add tests for OTP methods.

--- tests/OTPTest.php
+++ tests/OTPTest.php
@@ -2,12 +2,14 @@
 
 namespace yii2-otputil\test\OTP;
 
+use chillerlan\Authenticator\Authenticator;
 use chillerlan\Authenticator\Base32;
 use mad\otputil\OTP;
 use mad\otputil\Secret;
 
 class SecretTest extends TestCase
 {
+    protected $auth;
     protected $base32;
     protected $s;
     protected $p;
@@ -15,6 +17,7 @@ class SecretTest extends TestCase
     protected function setUp()
     {
         $this->mockApplication();
+        $this->auth = new Authenticator();
         $this->base32 = new Base32();
         $this->s = new Secret();
         $this->p = new OTP();
@@ -22,9 +25,34 @@ class SecretTest extends TestCase
 
     // Tests:
 
-    public function testGetCheckOTP()
+    public function testGetOTP()
     {
-        // to be implemented:
-        // generate one OTP and check it
+        $sid = $this->s->genSecret();
+        $secret = $this->s->getSecret($sid);
+
+        $totp = $this->p->getTOTP($sid);
+        $this->assertEquals(6, strlen($totp));
+        $this->assertStringMatchesFormat('%d', $totp);
+
+        $this->auth->setSecret($secret);
+        $code = $this->auth->code();
+        $this->assertEquals($code, $totp);
+
+        $chk = $this->auth->verify($totp);
+        $this->assertTrue($chk);
+    }
+
+    public function testCheckOTP()
+    {
+        $this->auth->createSecret();
+        $secret = $this->auth->getSecret();
+        $totp = $this->auth->code();
+        $sid = $this->s->setSecret($secret);
+        $chk = $this->p->checkTOTP($sid, $totp);
+        $this->assertTrue($chk);
+
+        $totp += 100;
+        $chk = $this->p->checkTOTP($totp);
+        $this->assertNotTrue($chk);
     }
 }
