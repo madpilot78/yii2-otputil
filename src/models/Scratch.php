5de91c78e54b68d24c5ecc35da3196b2fd075b7c
Guido Falsi <mad@madpilot.net>
2018-02-15 14:32:13 +0100

    Create a skeleton Scratch class.

---
+++ src/models/Scratch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace mad\otputil\models;
+
+use Yii;
+use mad\otputil\models\Secret;
+
+/**
+ * Model class for OTP scratch codes.
+ *
+ * @property integer $id
+ * @property string $code
+ * @property integer $secret_id
+ */
+class Scratch extends \yii\db\ActiveRecord
+{
+    /**
+     * @const DEFAULT_CODES Default number of codes to generate with automatic method
+     */
+    const DEFAULT_CODES = 5;
+
+    /**
+     * @var string the name of the DB connection to be used by this class
+     *
+     * NOTE: make configurable
+     */
+    private $db = 'db';
+
+    /**
+     * Initializes the application component.
+     * This method overrides the parent implementation by establishing the database connection.
+     */
+    public function init()
+    {
+        parent::init();
+        $this->db = Instance::ensure($this->db, Connection::className());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%otputil_scodes}}';
+    }
+
+    public function rules()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Forbid modifying records
+     */
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert)) {
+            return false;
+        }
+
+        if (!$insert) {
+            return false;
+        }
+
+        return true;
+    }
+}
