c2dad20b79247de7f462802df48a57aedbf8ef8a
Guido Falsi <mad@madpilot.net>
2018-02-15 19:02:36 +0100

    Add needed methods.

--- src/models/Scratch.php
+++ src/models/Scratch.php
@@ -102,4 +102,107 @@ class Scratch extends \yii\db\ActiveRecord
 
         return $code;
     }
+
+    /**
+     * Utility function to validate Secret IDs
+     *
+     * @param int $sid The Secret ID to be validated
+     * @return bool if validation succeeded
+     */
+    protected static function validateSID(int $sid)
+    {
+        $validator = new yii\validators\ExistsValidator();
+        $validator->targetClass = '\mad\otputil\models\Secret';
+        $validator->targetAttribute = '\mad\otputil\models\Secret';
+
+        return $validator->validate($sid, $error);
+    }
+
+    /**
+     * Creates $num scratch codes
+     *
+     * @param int $sid ID to assign the codes to
+     * @param int $num Number of codes to generate
+     * @return array of created AR objects
+     */
+    public static function createScratches(int $sid, int $num = self::DEFAULT_CODES)
+    {
+        if (!self::validateSID($sid)) {
+            return false;
+        }
+
+        $ret = [];
+        while ($num > 0) {
+            $t = new self($sid);
+            $t->save();
+            $ret[] = $t;
+            $num--;
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Verify a scratch code
+     *
+     * @param int $sid ID of secret to validate against
+     * @param string $code Scratch code to validate
+     * @param bool $del if the code should be deleted after successful check
+     * @return bool if verification was successful
+     */
+    public static function verifyCode(string $code, int $sid, bool $del = true)
+    {
+        if (!self::validateSID($sid)) {
+            return false;
+        }
+
+        $ret = false;
+        $valid_codes = self::findAll(['secret_id' => $sid]);
+
+        foreach ($valid_codes as $c) {
+            if ($c->code === $code)
+            {
+                $ret = true;
+                if ($del)
+                    $c->delete();
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Validate a scratch code using the static method, just populate the sid from the object.
+     *
+     * @param int $sid ID of secret to validate against
+     * @param string $code Scratch code to validate
+     * @param bool $del if the code should be deleted after successful check
+     * @return bool if verification was successful
+     */
+    public function verify(string $code, bool $del = true)
+    {
+        if (!self::validateSID($this->secret_id))
+            return false;
+
+        return self::verifyCode($code, $this->sid, $del);
+    }
+
+    /**
+     * Remove all codes assigned to a secret.
+     *
+     * @param int $sid pass to remove all codes bound to this secret ID
+     * @return bool success/failure
+     */
+    public static function remove(int $sid)
+    {
+        if (!self::validateSID($sid)) {
+            return false;
+        }
+
+        $codes = self::findAll(['secret_id' => $sid]);
+        foreach ($codes as $c)
+            $c->delete();
+
+        return true;
+    }
 }
