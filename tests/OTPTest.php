a463b753fabe7930a573ea101388329b0383ee77
Guido Falsi <mad@madpilot.net>
2018-02-20 09:35:45 +0100

    Secret object should be unnecessary here.

--- tests/OTPTest.php
+++ tests/OTPTest.php
@@ -5,7 +5,6 @@ namespace mad\otputil\tests;
 use chillerlan\Authenticator\Authenticator;
 use chillerlan\Authenticator\Base32;
 use mad\otputil\OTP;
-use mad\otputil\models\Secret;
 
 class SecretTest extends TestCase
 {
