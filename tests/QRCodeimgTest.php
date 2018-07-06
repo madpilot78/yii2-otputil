b352494d238bc960eed81e797b9b9130e5fd15fc
Guido Falsi <mad@madpilot.net>
2018-07-06 17:20:20 +0200

    Add a bunch of tests to better cover the widget code.

--- tests/QRCodeimgTest.php
+++ tests/QRCodeimgTest.php
@@ -6,6 +6,7 @@ use chillerlan\Authenticator\Base32;
 use Zxing\QrReader;
 use madpilot78\otputil\models\Secret;
 use madpilot78\otputil\widgets\QRCodeimg;
+use yii\web\ServerErrorHttpException;
 
 class QRCodeimgTest extends TestCase
 {
@@ -78,5 +79,146 @@ class QRCodeimgTest extends TestCase
         $qrreader = new QrReader($imgdata, QrReader::SOURCE_TYPE_BLOB);
 
         $this->assertEquals($exp, $qrreader->text());
+    }
+
+    /**
+     * Test wrong image format causes exception
+     *
+     * @return void
+     */
+    public function testWrongImageFormatException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $base32 = new Base32();
+        $testsecret = $base32->fromString(random_bytes(20));
+
+        $s = new Secret();
+        $s->secret = $testsecret;
+        $s->digits = 6;
+        $s->mode = 'totp';
+        $s->algo = 'SHA1';
+        $s->period = 30;
+        $s->save();
+
+        $img = QRCodeimg::widget([
+            'sid' => $s->id,
+            'fmt' => 'unkn',
+            'username' => 'foo',
+            'label' => 'bar'
+        ]);
+    }
+
+    /**
+     * Test wrong error correction level causes exception
+     *
+     * @return void
+     */
+    public function testWrongecLevelException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $base32 = new Base32();
+        $testsecret = $base32->fromString(random_bytes(20));
+
+        $s = new Secret();
+        $s->secret = $testsecret;
+        $s->digits = 6;
+        $s->mode = 'totp';
+        $s->algo = 'SHA1';
+        $s->period = 30;
+        $s->save();
+
+        $img = QRCodeimg::widget([
+            'sid' => $s->id,
+            'ecLevel' => 'X',
+            'username' => 'foo',
+            'label' => 'bar'
+        ]);
+    }
+
+    /**
+     * Test username with colon causes exception
+     *
+     * @return void
+     */
+    public function testColonInUsernameException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $base32 = new Base32();
+        $testsecret = $base32->fromString(random_bytes(20));
+
+        $s = new Secret();
+        $s->secret = $testsecret;
+        $s->digits = 6;
+        $s->mode = 'totp';
+        $s->algo = 'SHA1';
+        $s->period = 30;
+        $s->save();
+
+        $img = QRCodeimg::widget([
+            'sid' => $s->id,
+            'username' => 'f:oo',
+            'label' => 'bar'
+        ]);
+    }
+
+    /**
+     * Test label with colon causes exception
+     *
+     * @return void
+     */
+    public function testColonInLabelException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $base32 = new Base32();
+        $testsecret = $base32->fromString(random_bytes(20));
+
+        $s = new Secret();
+        $s->secret = $testsecret;
+        $s->digits = 6;
+        $s->mode = 'totp';
+        $s->algo = 'SHA1';
+        $s->period = 30;
+        $s->save();
+
+        $img = QRCodeimg::widget([
+            'sid' => $s->id,
+            'username' => 'foo',
+            'label' => 'ba:r'
+        ]);
+    }
+
+    /**
+     * Test wrong sid causes exception
+     *
+     * @return void
+     */
+    public function testWrongSidException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $img = QRCodeimg::widget([
+            'sid' => 42,
+            'username' => 'foo',
+            'label' => 'bar'
+        ]);
+    }
+
+    /**
+     * Test null sid causes exception
+     *
+     * @return void
+     */
+    public function testNullSidException()
+    {
+        $this->expectException(ServerErrorHttpException::class);
+
+        $img = QRCodeimg::widget([
+            'username' => 'foo',
+            'label' => 'bar'
+        ]);
     }
 }
